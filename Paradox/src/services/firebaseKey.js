7669547f4ba9801e4239f000913d585c989fab3b
JDAX8 <juanampudia888@gmail.com>
2024-05-31 17:39:05 -0500

    Commit 1

---
+++ Paradox/src/services/firebaseKey.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAPTo86De7NqbrTqtWgUQwA0MWvKOHw-m8",
+    authDomain: "paradox-eced6.firebaseapp.com",
+    projectId: "paradox-eced6",
+    storageBucket: "paradox-eced6.appspot.com",
+    messagingSenderId: "647375547341",
+    appId: "1:647375547341:web:cd228238e7a8b0568cf284",
+    measurementId: "G-QEYD8XZXPC"
+  };
+export default firebaseConfig;
